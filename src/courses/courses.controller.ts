74abf0c0443b1513a6ee8483ac4845ee4f912c9b
unknown <sam.nuntawat@gmail.com>
2025-04-09 20:32:23 +0700

    fix syntax

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,9 @@
+import { Controller } from '@nestjs/common';
+import { CoursesService } from './courses.service';
+
+@Controller('courses')
+export class CoursesController {
+    constructor(private readonly coursesService:CoursesService){}
+
+    
+}
